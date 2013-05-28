dac19e39a0fe4e3364381c23e862a0d39786949f
Jurian Sluiman <jurian@soflomo.com>
2013-05-28 15:00:57 +0200

    Remove beanstalkd exceptions

--- src/SlmQueueBeanstalkd/Worker/Worker.php
+++ src/SlmQueueBeanstalkd/Worker/Worker.php
@@ -7,7 +7,6 @@ use Pheanstalk_Pheanstalk as Pheanstalk;
 use SlmQueue\Job\JobInterface;
 use SlmQueue\Queue\QueueInterface;
 use SlmQueue\Worker\AbstractWorker;
-use SlmQueueBeanstalkd\Job\Exception as JobException;
 use SlmQueueBeanstalkd\Queue\TubeInterface;
 
 /**
@@ -27,10 +26,6 @@ class Worker extends AbstractWorker
         try {
             $job->execute();
             $queue->delete($job);
-        } catch(JobException\ReleasableException $exception) {
-            $queue->release($job, $exception->getOptions());
-        } catch (JobException\BuryableException $exception) {
-            $queue->bury($job, $exception->getOptions());
         } catch (Exception $exception) {
             $queue->bury($job, array('priority' => Pheanstalk::DEFAULT_PRIORITY));
         }
