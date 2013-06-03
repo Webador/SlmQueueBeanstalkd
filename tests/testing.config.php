b32bb221ac79da6a55dbde211f10654b8843b6e2
Jurian Sluiman <jurian@soflomo.com>
2013-06-03 14:57:31 +0200

    Rename classes in tests

--- tests/testing.config.php
+++ tests/testing.config.php
@@ -25,7 +25,7 @@ return array(
 
         'queue_manager' => array(
             'factories' => array(
-                'newsletter' => 'SlmQueueBeanstalkd\Factory\TubeFactory'
+                'newsletter' => 'SlmQueueBeanstalkd\Factory\BeanstalkdQueueFactory'
             )
         )
     )
