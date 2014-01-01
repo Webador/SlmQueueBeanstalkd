d7fe967c2330ded84f769b6fc2dfa526b7048219
Michaël Gallego <mic.gallego@gmail.com>
2014-01-01 19:04:20 +0100

    Options

--- src/SlmQueueBeanstalkd/Factory/TubeFactory.php
+++ src/SlmQueueBeanstalkd/Factory/TubeFactory.php
@@ -2,6 +2,7 @@
 
 namespace SlmQueueBeanstalkd\Factory;
 
+use SlmQueueBeanstalkd\Options\TubeOptions;
 use SlmQueueBeanstalkd\Queue\Tube;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -20,7 +21,14 @@ class TubeFactory implements FactoryInterface
         $pheanstalk       = $parentLocator->get('SlmQueueBeanstalkd\Service\PheanstalkService');
         $jobPluginManager = $parentLocator->get('SlmQueue\Job\JobPluginManager');
 
-        // Check if the user has specified options for the given queues
+        // Check if the user has specified options for the given queues, or create a default one
+        $queuesOptions = $parentLocator->get('SlmQueue\Options\ModuleOptions')->getQueues();
+
+        if (isset($queuesOptions[$requestedName])) {
+            $options = new TubeOptions($queuesOptions[$requestedName]);
+        } else {
+            $options = new TubeOptions();
+        }
 
         return new Tube($pheanstalk, $options, $requestedName, $jobPluginManager);
     }
