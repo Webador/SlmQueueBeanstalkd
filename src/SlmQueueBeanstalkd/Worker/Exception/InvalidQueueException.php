13b83ed7498094d082fc9be9b729493f9d43dbae
Jurian Sluiman <jurian@soflomo.com>
2014-04-21 11:33:44 +0200

    Use the new Beanstalkd ExceptionInterface

--- src/SlmQueueBeanstalkd/Worker/Exception/InvalidQueueException.php
+++ src/SlmQueueBeanstalkd/Worker/Exception/InvalidQueueException.php
@@ -2,7 +2,7 @@
 
 namespace SlmQueueBeanstalkd\Worker\Exception;
 
-use SlmQueue\Exception\ExceptionInterface;
+use SlmQueueBeanstalkd\Exception\ExceptionInterface;
 use InvalidArgumentException;
 
 class InvalidQueueException extends InvalidArgumentException implements ExceptionInterface
