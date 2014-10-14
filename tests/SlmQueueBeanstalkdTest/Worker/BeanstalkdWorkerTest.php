5d6dadeec0ddc2d7869a74c49eb95c09615d1ad2
Jurian Sluiman <jurian@soflomo.com>
2014-10-14 21:05:20 +0200

    Update Worker to deal with events

--- tests/SlmQueueBeanstalkdTest/Worker/BeanstalkdWorkerTest.php
+++ tests/SlmQueueBeanstalkdTest/Worker/BeanstalkdWorkerTest.php
@@ -2,84 +2,60 @@
 
 namespace SlmQueueBeanstalkdTest\Worker;
 
-use Exception;
 use PHPUnit_Framework_TestCase as TestCase;
-use SlmQueueBeanstalkdTest\Asset;
+use SlmQueue\Worker\WorkerEvent;
 use SlmQueueBeanstalkd\Worker\BeanstalkdWorker;
-use SlmQueueBeanstalkdTest\Util\ServiceManagerFactory;
-use Zend\ServiceManager\ServiceManager;
 
 class BeanstalkdWorkerTest extends TestCase
 {
-    /**
-     * @var ServiceManager
-     */
-    protected $serviceManager;
-
-    /**
-     * @var \SlmQueueBeanstalkd\Queue\BeanstalkdQueueInterface
-     */
-    protected $queueMock;
-
     /**
      * @var BeanstalkdWorker
      */
     protected $worker;
 
-
     public function setUp()
     {
-        parent::setUp();
-        $this->serviceManager = ServiceManagerFactory::getServiceManager();
-
-        $this->queueMock  = $this->getMock('SlmQueueBeanstalkd\Queue\BeanstalkdQueueInterface');
-        $queueManagerMock = $this->getMock('SlmQueue\Queue\QueuePluginManager');
-        $workerOptions    = $this->serviceManager->get('SlmQueue\Options\WorkerOptions');
-
-        $this->worker = new BeanstalkdWorker($queueManagerMock, $workerOptions);
+        $this->worker = new BeanstalkdWorker($this->getMock('Zend\EventManager\EventManagerInterface'));
     }
 
-    public function testAssertJobIsDeletedIfNoExceptionIsThrown()
-    {
-        $job = new Asset\SimpleJob();
-
-        $this->queueMock->expects($this->once())
-            ->method('delete')
-            ->will($this->returnCallback(function() use ($job) {
-                $job->setContent('deleted');
-            })
-        );
-
-        $this->worker->processJob($job, $this->queueMock);
-
-        $this->assertEquals('deleted', $job->getContent());
-    }
-
-    public function testAssertJobIsBuriedIfAnyExceptionIsThrown()
-    {
-        $job = new Asset\ExceptionJob();
-
-        $this->queueMock->expects($this->once())
-            ->method('bury')
-            ->will($this->returnCallback(function() use ($job) {
-                $job->setContent('buried');
-            })
-        );
-
-        $this->worker->processJob($job, $this->queueMock);
-
-        $this->assertEquals('buried', $job->getContent());
-    }
-
-    public function testOnlyBeanstalkdQueuesAreAllowed()
+    public function testReturnsUnknownIfNotABeanstalkdQueue()
     {
         $queue = $this->getMock('SlmQueue\Queue\QueueInterface');
-        $job   = new Asset\SimpleJob();
+        $job   = $this->getMock('SlmQueue\Job\JobInterface');
 
-        try {
-            $this->worker->processJob($job, $queue);
-        } catch (\Exception $e) {
-            $this->assertInstanceOf('SlmQueueBeanstalkd\Exception\ExceptionInterface', $e);
-        }
+        $status = $this->worker->processJob($job, $queue);
+        $this->assertEquals(WorkerEvent::JOB_STATUS_UNKNOWN, $status);
+    }
+
+    public function testDeleteJobOnSuccess()
+    {
+        $queue = $this->getMock('SlmQueueBeanstalkd\Queue\BeanstalkdQueueInterface');
+        $job   = $this->getMock('SlmQueue\Job\JobInterface');
+
+        $job->expects($this->once())
+            ->method('execute');
+
+        $queue->expects($this->once())
+              ->method('delete')
+              ->with($job);
+
+        $status = $this->worker->processJob($job, $queue);
+        $this->assertEquals(WorkerEvent::JOB_STATUS_SUCCESS, $status);
+    }
+
+    public function testDoNotDeleteJobOnFailure()
+    {
+        $queue = $this->getMock('SlmQueueBeanstalkd\Queue\BeanstalkdQueueInterface');
+        $job   = $this->getMock('SlmQueue\Job\JobInterface');
+
+        $job->expects($this->once())
+            ->method('execute')
+            ->will($this->throwException(new \RuntimeException()));
+
+        $queue->expects($this->never())
+              ->method('delete');
+
+        $status = $this->worker->processJob($job, $queue);
+        $this->assertEquals(WorkerEvent::JOB_STATUS_FAILURE_RECOVERABLE, $status);
     }
 }
