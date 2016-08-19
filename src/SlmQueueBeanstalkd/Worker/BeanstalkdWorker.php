27e3d24aa86fc654945f3c4ffac9d453655c0b9d
Joe Green <joe.green@liquidhealthcare.com>
2016-08-19 15:37:05 +0100

    Update BeanstalkdWorker to use ProcessJob event instead of removed WorkerEvent

--- src/SlmQueueBeanstalkd/Worker/BeanstalkdWorker.php
+++ src/SlmQueueBeanstalkd/Worker/BeanstalkdWorker.php
@@ -6,7 +6,7 @@ use Exception;
 use SlmQueue\Job\JobInterface;
 use SlmQueue\Queue\QueueInterface;
 use SlmQueue\Worker\AbstractWorker;
-use SlmQueue\Worker\WorkerEvent;
+use SlmQueue\Worker\Event\ProcessJobEvent;
 use SlmQueueBeanstalkd\Queue\BeanstalkdQueueInterface;
 
 /**
@@ -20,7 +20,7 @@ class BeanstalkdWorker extends AbstractWorker
     public function processJob(JobInterface $job, QueueInterface $queue)
     {
         if (!$queue instanceof BeanstalkdQueueInterface) {
-            return WorkerEvent::JOB_STATUS_UNKNOWN;
+            return ProcessJobEvent::JOB_STATUS_UNKNOWN;
         }
 
         /**
@@ -33,10 +33,10 @@ class BeanstalkdWorker extends AbstractWorker
             $job->execute();
             $queue->delete($job);
 
-            return WorkerEvent::JOB_STATUS_SUCCESS;
+            return ProcessJobEvent::JOB_STATUS_SUCCESS;
         } catch (Exception $exception) {
             // Do nothing, the job will be reinserted automatically for another try
-            return WorkerEvent::JOB_STATUS_FAILURE_RECOVERABLE;
+            return ProcessJobEvent::JOB_STATUS_FAILURE_RECOVERABLE;
         }
     }
 }
